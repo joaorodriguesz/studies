c80af6fbc0164104bfd64737e3b34914051795aa
joao.rodrigues <joaopaulomoreirarodrigues@gmail.com>
2023-08-03 15:51:09 -0300

    java: data validate spring-boot api

---
+++ java/spring-boot/api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,43 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.endereco.Endereco;
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico medico) {
+        this.nome = medico.nome();
+        this.email = medico.email();
+        this.telefone = medico.telefone();
+        this.crm = medico.crm();
+        this.especialidade = medico.especialidade();
+        this.endereco = new Endereco(medico.endereco());
+    }
+}
