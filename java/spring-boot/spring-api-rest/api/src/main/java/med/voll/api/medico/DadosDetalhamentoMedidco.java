69ac1f5b79307e3803f02ae026f5baafc3a28d7c
joao.rodrigues <joaopaulomoreirarodrigues@gmail.com>
2023-08-10 16:03:47 -0300

    spring: spring security

--- java/spring-boot/spring-api-rest/api/src/main/java/med/voll/api/medico/DadosDetalhamentoMedidco.java
+++
@@ -1,9 +0,0 @@
-package med.voll.api.medico;
-
-import med.voll.api.endereco.Endereco;
-
-public record DadosDetalhamentoMedidco(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
-    public DadosDetalhamentoMedidco(Medico medico){
-        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
-    }
-}
